c88ba07759d12f78f1f5c283d5f1bc1724bb7704
Celhammouti <chahid.el-hammouti@eleve.ensai.fr>
2025-10-03 10:00:26 +0000

    update AddressDAO + CustomerDAO

--- src/DAO/AddressDAO.py
+++ src/DAO/AddressDAO.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-from dao.db_connection import DBConnection
-
 from src.Model.Address import Address
 
 from .DBConnector import DBConnector
@@ -13,56 +11,84 @@ class AddressDAO:
     def __init__(self, db_connector: DBConnector):
         self.db_connector = db_connector
 
-    def get_address(self, customer_id) -> Address:
+    def get_address(self, customer_id) -> Optional[Address]:
         """Get the Adress of one customer thanks to customer_id"""
-        with DBConnection().connection as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(
-                    "SELECT *        "
-                    "FROM Address   "
-                    "JOIN Customer                          "
-                    "USING address_id                          "
-                    "WHERE customer_id = %(customer_id)s                          ",
-                    {"customer_id": customer_id},
-                )
-                address_bdd = cursor.fetchone()
-
-        address = None
-        if address_bdd:
-            address = Address(
-                number=address_bdd["number"],
-                street=address_bdd["street"],
-                city=address_bdd["city"],
-                postal_code=address_bdd["postal_code"],
-                country=address_bdd["country"],
+        raw_address = self.db_connector.sql_query(
+            "SELECT *        "
+            "FROM Address   "
+            "JOIN Customer                          "
+            "USING address_id                          "
+            "WHERE customer_id = %(customer_id)s                          ",
+            {"customer_id": customer_id},
+            "one",
+        )
+        if raw_address:
+            raw_address = Address(
+                number=raw_address["number"],
+                street=raw_address["street"],
+                city=raw_address["city"],
+                postal_code=raw_address["postal_code"],
+                country=raw_address["country"],
             )
-        return address
+        return raw_address
 
-    def get_all_addresses(self) -> list[Address]:
+    def get_all_addresses(self) -> Optional[list[Address]]:
         """Get all addresses"""
-        with DBConnection().connection as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT *                     FROM Address ;                    ")
-                address_bdd = cursor.fetchall()
+        address_bdd = self.db_connector.sql_query("SELECT * FROM Address", "all")
 
         list_address = []
 
-        if address_bdd:
-            for address in address_bdd:
-                list_address.append(
-                    Address(
-                        number=address["number"],
-                        street=address["street"],
-                        city=address["city"],
-                        postal_code=address["postal_code"],
-                        country=address["country"],
-                    )
+        if address_bdd is None:
+            return None
+        for address in address_bdd:
+            list_address.append(
+                Address(
+                    number=address["number"],
+                    street=address["street"],
+                    city=address["city"],
+                    postal_code=address["postal_code"],
+                    country=address["country"],
                 )
+            )
 
         return list_address
 
-        def create_address(self, address_id, number, street, city, postal_code, country)-> bool :
-            #TO DO
+    def create_address(self, address_id, number, street, city, postal_code, country) -> Address:
+        raw_created_address = self.db_connector.sql_query(
+            """
+        INSERT INTO Address (address_id, number, street, city, postal_code, country)
+        VALUES (DEFAULT, %(number)s,%(street)s, %(city)s, %(postal_code)s, %(country)s)
+        RETURNING *;
+        """,
+            {"key": 1},
+            "one",
+        )
+        return Address(**raw_created_address)
 
+    def update_address(self, address_id, number, street, city, postal_code, country):
+        raw_update_address = self.db_connector.sql_query(
+            """
+        UPDATE Address SET number = %(number)s, street=%(street)s, city=%(city)s, postal_code=%(postal_code)s, country=%(country)s
+        WHERE address_id=%(address_id)s RETURNING *;
+        """,
+            {"key": 1},
+            "one",
+        )
+        return Address(**raw_update_address)
 
-
+    def delete_address_by_customer(self, customer_id):
+        raw_delete_address = self.db_connector.sql_query(
+            """
+        DELETE FROM Address JOIN Customers Using (address_id) WHERE customer_id=%s
+        """,
+            "one",
+        )
+        if raw_delete_address:
+            raw_delete_address = Address(
+                number=raw_delete_address["number"],
+                street=raw_delete_address["street"],
+                city=raw_delete_address["city"],
+                postal_code=raw_delete_address["postal_code"],
+                country=raw_delete_address["country"],
+            )
+        return raw_delete_address
--- src/DAO/CustomerDAO.py
+++ src/DAO/CustomerDAO.py
@@ -11,9 +11,49 @@ class CustomerDAO:
     def __init__(self, db_connector: DBConnector):
         self.db_connector = db_connector
 
-    def get_customer_by_id(self, customer_id):
+    def get_customer_by_id(self, customer_id) -> Optional[Customer]:
         raw_customer = self.db_connector.sql_query("SELECT * from Customers WHERE id=%s", [customer_id], "one")
         if raw_customer is None:
             return None
         # pyrefly: ignore
         return Customer(**raw_customer)
+
+    def get_all_customer(self) -> Optional[list[Customer]]:
+        raw_customers = self.db_connector.sql_query("SELECT * from Customers ", "all")
+
+        if raw_customers is None:
+            return None
+
+        list_customer = [Customer(**customer) for customer in raw_customers]
+        return list_customer
+
+    def create_customer(self, customer_id, first_name, last_name, phone, mail, password_hash, address_id) -> Customer:
+        raw_created_customer = self.db_connector.sql_query(
+            """
+        INSERT INTO Address (customer_id, first_name, last_name, phone, mail, password_hash, address_id)
+        VALUES (DEFAULT, %(first_name)s,%(last_name)s, %(phone)s, %(mail)s, %(password_hash)s, %(address_id)s )
+        RETURNING *;
+        """,
+        )
+        return Customer(**raw_created_customer)
+
+    def update_customer(self, customer_id, first_name, last_name, phone, mail, password_hash, address_id):
+        raw_update_customer = self.db_connector.sql_query(
+            """
+        UPDATE Address SET first_name = %(first_name)s, last_name=%(last_name)s, phone=%(phone)s, mail=%(mail)s,
+        password_hash=%(password_hash)s, address_id=%(address_id)s
+        WHERE customer_id=%(customer_id)s RETURNING *;
+        """,
+            {"key": 1},
+            "one",
+        )
+        return Customer(**raw_update_customer)
+
+    def delete_customer(self, customer_id):
+        raw_delete_customer = self.db_connector.sql_query(
+            """
+        DELETE FROM Customers WHERE customer_id=%s
+        """,
+            "one",
+        )
+        return Customer(**raw_delete_customer)
