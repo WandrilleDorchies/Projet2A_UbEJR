1bf068598c7460354b21ed06b8776382e484a9e8
Celhammouti <chahid.el-hammouti@eleve.ensai.fr>
2025-10-06 17:31:17 +0000

    doc AddressDAO

--- src/DAO/AddressDAO.py
+++ src/DAO/AddressDAO.py
@@ -13,7 +13,21 @@ class AddressDAO(metaclass=Singleton):
         self.db_connector = db_connector
 
     def get_address(self, customer_id) -> Optional[Address]:
-        """Get the Adress of one customer thanks to customer_id"""
+        """
+        Retrieve the address associated with a given customer.
+
+        Args
+        ----
+        customer_id (int):
+            Unique identifier of the customer whose address is requested.
+
+        Returns
+        -------
+        Address or None
+            An Address object containing the customer's address information
+            if found in the database, None otherwise.
+        """
+
         raw_address = self.db_connector.sql_query(
             "SELECT *        "
             "FROM Address   "
@@ -34,7 +48,16 @@ class AddressDAO(metaclass=Singleton):
         return raw_address
 
     def get_all_addresses(self) -> Optional[list[Address]]:
-        """Get all addresses"""
+        """
+        Retrieve all addresses stored in the database.
+
+        Returns
+        -------
+        list[Address] or None
+            A list of Address objects if at least one address exists in the database,
+            None otherwise.
+        """
+
         address_bdd = self.db_connector.sql_query("SELECT * FROM Address", "all")
 
         list_address = []
@@ -55,6 +78,30 @@ class AddressDAO(metaclass=Singleton):
         return list_address
 
     def create_address(self, address_id, number, street, city, postal_code, country) -> Address:
+        """
+        Create a new address entry in the database.
+
+        Args
+        ----
+        address_id (int):
+            Unique identifier for the address (ignored in insertion since set to DEFAULT).
+        number (str or int):
+            Street number of the address.
+        street (str):
+            Name of the street.
+        city (str):
+            City where the address is located.
+        postal_code (str or int):
+            Postal code of the address.
+        country (str):
+            Country of the address.
+
+        Returns
+        -------
+        Address
+            The newly created Address object containing the inserted information.
+        """
+
         raw_created_address = self.db_connector.sql_query(
             """
         INSERT INTO Address (address_id, number, street, city, postal_code, country)
@@ -67,6 +114,30 @@ class AddressDAO(metaclass=Singleton):
         return Address(**raw_created_address)
 
     def update_address(self, address_id, number, street, city, postal_code, country):
+        """
+        Update an existing address in the database.
+
+        Args
+        ----
+        address_id (int):
+            Unique identifier of the address to update.
+        number (str or int):
+            Updated street number of the address.
+        street (str):
+            Updated street name.
+        city (str):
+            Updated city name.
+        postal_code (str or int):
+            Updated postal code.
+        country (str):
+            Updated country name.
+
+        Returns
+        -------
+        Address
+            The updated Address object reflecting the new information stored in the database.
+        """
+
         raw_update_address = self.db_connector.sql_query(
             """
         UPDATE Address SET number = %(number)s, street=%(street)s, city=%(city)s,
@@ -79,6 +150,20 @@ class AddressDAO(metaclass=Singleton):
         return Address(**raw_update_address)
 
     def delete_address_by_customer(self, customer_id):
+        """
+        Delete the address associated with a given customer from the database.
+
+        Args
+        ----
+        customer_id (int):
+            Unique identifier of the customer whose address should be deleted.
+
+        Returns
+        -------
+        Address or None
+            The deleted Address object if the operation was successful, None otherwise.
+        """
+
         raw_delete_address = self.db_connector.sql_query(
             """
         DELETE FROM Address JOIN Customers Using (address_id) WHERE customer_id=%s
