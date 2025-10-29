a524102a70e1f10b58f4729b2889a28ea54f24ea
wandrilled <wandrille.dorchies@eleve.ensai.fr>
2025-10-29 13:18:12 +0000

    doc: questions/suggestions and doc on AuthController

--- src/App/AuthentificationController.py
+++ src/App/AuthentificationController.py
@@ -18,6 +18,46 @@ def register(
     confirm_password: str,
     address_string: str,
 ):
+    # Suggestion: registration has a field for each "component" of the address
+    # Suggestion: login confirmation message using the returned dict
+    # Question: what is expected to be caught in the ValueError and in the last Exception ?
+    #           Why is is 2 different errors? Is ValueError for stuff caught in CustomerService
+    #           And Exception for anything else ?
+    # Question: why return the customer object content and the JWT ?
+    #           what are they used for afterwards ?
+    """
+    Registration request for customers
+
+    Parameters
+    ----------
+    first_name : str
+        first name of the customer
+    last_name : str
+        last name of the customer
+    phone : str
+        phone number of the customer. Phone number validity is checked by CustomerService.
+    mail : str
+        email address of the customer
+    password : str
+        Password [TODO: specify requirements]
+    confirm_password : str
+        Password confirmation, must match password
+    address_string : str
+        Address of the customer as a string. Address validity is cheked by CustomerService.
+
+    Returns
+    -------
+    _type_: ?
+        _description_ : ?
+    Raises
+    ------
+    HTTPException
+        raised if password confimration field differs from password field
+    HTTPException
+        _description_ ?
+    HTTPException
+        _description_ ?
+    """
     try:
         if confirm_password != password:
             raise HTTPException(status_code=400, detail="The two password don't match.")
@@ -49,6 +89,35 @@ def register(
 def login(
     identifier: str, password: str, user_type: Literal["admin", "customer", "driver"]
 ) -> JWTResponse:
+    # Suggestion: identifier of type Optional[str] as admin only logs in with pw
+    """
+    Login request for users regardless of type
+
+    Parameters
+    ----------
+    identifier : Optional[str]
+        For customers : email address or phone number
+        For drivers : phone number
+        For admins : None
+    password : str
+        Password of the user
+    user_type : Literal[admin, customer, driver]
+        Type of user
+
+    Returns
+    -------
+    JWTResponse
+       JWT token to allow further actions without re-login
+
+    Raises
+    ------
+    HTTPException
+        raised if invalid user type
+    HTTPException
+        raised if invalid credentials
+    HTTPException
+        other exceptions
+    """
     try:
         if user_type not in ("admin", "customer", "driver"):
             raise HTTPException(
