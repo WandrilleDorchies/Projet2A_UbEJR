f27ab25fb8d311bafd9de5d991aec4c270de827f
martinlepadan <id2653@ensai.fr>
2025-11-20 18:07:51 +0000

    fix overview

--- src/App/AdminController/AdminController.py
+++ src/App/AdminController/AdminController.py
@@ -9,6 +9,7 @@ from src.App.init_app import (
     order_service,
 )
 from src.App.JWTBearer import AdminBearer
+from src.Model.Order import OrderState
 
 templates = Jinja2Templates(directory="templates")
 admin_router = APIRouter(prefix="", tags=["General"], dependencies=[Depends(AdminBearer())])
@@ -22,20 +23,24 @@ def get_overview():
     try:
         customers = customer_service.get_all_customers()
         drivers = driver_service.get_all_drivers()
-        orders = order_service.get_all_orders()
-        orders_prepared = order_service.get_prepared_orders()
-        current_orders = order_service.get_current_orders()
-        benef = [order.order_price for order in order_service.get_past_orders()]
+
+        orders_prepared = order_service.get_orders_by_state(OrderState.PREPARED)
+        current_orders = order_service.get_orders_by_state(OrderState.PAID)
+        delivering_orders = order_service.get_orders_by_state(OrderState.DELIVERING)
+        delivered_order = order_service.get_orders_by_state(OrderState.DELIVERED)
+
+        past_orders = orders_prepared + current_orders + delivering_orders + delivered_order
+        benef = sum([order.order_price for order in list(past_orders)])
+
         items = menu_service.get_all_orderables(in_menu=True)
 
         return {
             "total_customers": len(customers) if customers else 0,
             "total_drivers": len(drivers) if drivers else 0,
-            "total_orders": len(orders) if orders else 0,
             "total_orders_prepared": len(orders_prepared) if orders_prepared else 0,
-            "total_current_orders": len(current_orders) if current_orders else 0,
+            "total_orders_in_kitchen": len(current_orders) if current_orders else 0,
             "total_orderables_in_menu": len(items) if items else 0,
-            "benefice": f"{sum(benef)} €",
+            "benefice": f"{benef} €",
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching datas: {e}") from e
